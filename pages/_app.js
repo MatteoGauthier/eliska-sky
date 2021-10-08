27e9173a813e3dbbf8d04276b798f23377336e0c
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-10-08 09:42:21 +0200

    Refacotring code to components , adding pier sans fonté

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'tailwindcss/dist/base.css'
-import '../styles/globals.css'
 import '../styles/fonts.css'
+import '../styles/globals.css'
+import '../styles/Biography.css'
+import '../styles/Footer.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
